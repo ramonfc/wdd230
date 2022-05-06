e32029f3f22d73792ba03fa5cd3146928243b562
ramonfc <monchouam@gmail.com>
2022-05-06 06:39:56 -0500

    added target page for small view

--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -1,6 +1,23 @@
+// To use with dates in footer:
 date = new Date();
 const year = document.querySelector("#currentYear");
 year.innerHTML  = date.getFullYear()
 
 const modified = document.querySelector("#modified");
 modified.innerHTML  = document.lastModified
+
+
+// To use with accordion in target page:
+let acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {    
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
