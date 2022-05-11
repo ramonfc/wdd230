386827ef7627bed146476713f093487e930e8df9
ramonfc <monchouam@gmail.com>
2022-05-11 12:29:20 -0500

    starting the hopage for chamber site

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerBtn");
+x.onclick = toggleMenu;
